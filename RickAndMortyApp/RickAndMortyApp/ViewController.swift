91984687e3a7045d96644c774b06ee8cdc5df88f
NatalliaDrozd <nat.dr000@gmail.com>
2023-08-17 19:22:05 +0300

    Initial project

---
+++ RickAndMortyApp/RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Natalia Drozd on 17.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
