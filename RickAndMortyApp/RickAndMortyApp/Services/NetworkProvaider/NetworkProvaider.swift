4e0c35566d04d1efb4720585777f07d1dadd7ac0
NatalliaDrozd <nat.dr000@gmail.com>
2023-08-20 16:08:18 +0300

    Added function for get new information

--- RickAndMortyApp/RickAndMortyApp/Services/NetworkProvaider/NetworkProvaider.swift
+++ RickAndMortyApp/RickAndMortyApp/Services/NetworkProvaider/NetworkProvaider.swift
@@ -10,6 +10,9 @@ import UIKit
 protocol NetworkProvaiderProtocol {
     func getcountFromUrl (completion: @escaping (CoutOfCharacters) -> Void)
     func getDataFromURL (count: Int, completion: @escaping ([InfoCharacters]) -> Void)
+    func getInfoById (id: Int, completion: @escaping (InfoCharacters) -> Void)
+    func getInfoEpisodes (url: String, completion: @escaping (InfoEpisodes) -> Void)
+    func getInfoOrigin (url: String, completion: @escaping (InfoOrigin) -> Void)
 }
 
 final class NetworkProvaider: NetworkProvaiderProtocol {
@@ -47,6 +50,70 @@ final class NetworkProvaider: NetworkProvaiderProtocol {
                 if let data = data {
                     do {
                         let info = try JSONDecoder().decode([InfoCharacters].self, from: data)
+                        completion(info)
+                    } catch {
+                        print (error.localizedDescription)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func getInfoById (id: Int, completion: @escaping (InfoCharacters) -> Void) {
+        if let url = URL(string: "\(Constants.baseUrl)\(id)") {
+            // print(url)
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "GET"
+            let task = URLSession.shared.dataTask(with: urlRequest) { [weak self] data, _, error in
+                guard self != nil else { return }
+                if error != nil {
+                }
+                if let data = data {
+                    do {
+                        let info = try JSONDecoder().decode(InfoCharacters.self, from: data)
+                        completion(info)
+                    } catch {
+                        print (error.localizedDescription)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func getInfoEpisodes (url: String, completion: @escaping (InfoEpisodes) -> Void) {
+        if let url = URL(string: url) {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "GET"
+            let task = URLSession.shared.dataTask(with: urlRequest) { [weak self] data, _, error in
+                guard self != nil else { return }
+                if error != nil {
+                }
+                if let data = data {
+                    do {
+                        let info = try JSONDecoder().decode(InfoEpisodes.self, from: data)
+                        completion(info)
+                    } catch {
+                        print (error.localizedDescription)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func getInfoOrigin (url: String, completion: @escaping (InfoOrigin) -> Void) {
+        if let url = URL(string: url) {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "GET"
+            let task = URLSession.shared.dataTask(with: urlRequest) { [weak self] data, _, error in
+                guard self != nil else { return }
+                if error != nil {
+                }
+                if let data = data {
+                    do {
+                        let info = try JSONDecoder().decode(InfoOrigin.self, from: data)
                         completion(info)
                     } catch {
                         print (error.localizedDescription)
